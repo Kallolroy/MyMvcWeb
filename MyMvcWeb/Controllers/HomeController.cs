9c983523952aad5948fc6f7d0d63b4ca7541fa7e
kroy14 <kallol.roy@sentrana.com>
2016-08-23 16:20:33 +0600

    modified

--- MyMvcWeb/Controllers/HomeController.cs
+++ MyMvcWeb/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MyMvcWeb.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "This is test again again kallol roy xxxxx";
+            ViewBag.Message = "This is test again again kallol roy yyyy";
 
             return View();
         }
