c23dd907904b00bd07b59883f2315de388845af7
kroy14 <kallol.roy@sentrana.com>
2016-08-25 13:01:50 +0600

    update test cases

--- MyMvcWeb.Tests/Controllers/HomeControllerTest.cs
+++ MyMvcWeb.Tests/Controllers/HomeControllerTest.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyMvcWeb.Controllers;
 
-namespace MvcApplication1.Tests.Controllers
+namespace MyMvcWeb.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
--- MyMvcWeb.Tests/Properties/AssemblyInfo.cs
+++ MyMvcWeb.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcApplication1.Tests")]
+[assembly: AssemblyTitle("MyMvcWeb.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MvcApplication1.Tests")]
+[assembly: AssemblyProduct("MyMvcWeb.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
